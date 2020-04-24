319b5cc04b023e6106383ff0141d740ea46ebd11
florian <flo@pop-os.localdomain>
2020-04-24 17:30:20 +0200

    ca avance

--- subs.py
+++ subs.py
@@ -1,19 +1,38 @@
 from srt_normalizer import *
   
-L = ["Geeks\n", "for\n", "Geeks\n"] 
-  
+def findWord(word, list):
+    count = 0
+    for s in list:
+        count = count+1
+        if s.find(word) !=-1 : #si on trouve le mot
+            return count
+    return -1
 
-#comment qu'on fait
-# on va faire deux tableaux. un avec les timestamp, un avec le texte. chauqe case correspont au numéro du sous titre. 
-# on parcourt les textes jusqu'à trouver le mot que l'on veut. quand on le trouve, on print la case. bien. 
-# Using readlines() 
+
 normalize("sw3.srt")
 file1 = open('sw3.txt', 'r') 
 Lines = file1.readlines() 
-for l in Lines:
-    print(l)
+quotes = [""]*len(Lines)
+timeStamp = [""]*len(Lines)
+count = 0 
+i=0
+#on parcourt tutes les lignes 
+while i<len(Lines)-5 :
+    #la premiere ligne c'est le numéro du sous titre, on s'en cogne
+    i=i+1
+    #on save le timestamp
+    timeStamp[count]=Lines[i]
+    i=i+1
+    #maintenant on save le texte
+    s=""
+    while Lines[i] != "\n" :
+        s2=Lines[i].replace("\n", " ") 
+        s=s+s2
+        i=i+1
+    quotes[count]=s
+    i=i+1
+    count=count+1
+    #et on recommence
 
-# Strips the newline character 
-#for line in Lines: 
-#    print(line.strip()) 
-#    print("Line{}: {}".format(count, line.strip())) 
+print(findWord("clones", quotes))
+
